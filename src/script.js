41d5f7e5a47539c07b29b8f1e6c6316ee4321deb
pav-nk <pav.nk.vh@gmail.com>
2024-05-30 16:10:02 +0300

    feat: basic functionality of the application has been developed

--- src/script.js
+++ src/script.js
@@ -1,3 +1,7 @@
+// nodes
+const form = document.body.querySelector(".entry-field");
+const booksContainer = document.body.querySelector(".library__items");
+
 const myLibrary = [];
 
 function Book() {
@@ -5,5 +9,36 @@ function Book() {
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const newBookData = Object.fromEntries(formData);
+    newBookData.id = myLibrary.length + 1;
+    myLibrary.push(newBookData);
+    console.log(myLibrary);
+    e.target.reset();
+    renderBooks();
+  });
 }
+
+function renderBooks() {
+  booksContainer.innerHTML = "";
+  for (const book of myLibrary) {
+    const { id, name, author, status } = book;
+    booksContainer.insertAdjacentHTML(
+      "beforeend",
+      `
+    <tr class="library__item">
+      <td>${name}</td>
+      <td>${author}</td>
+      <td><span class="read-status">${
+        status === "1" ? "read" : "unread"
+      }</span></td>
+      <td><button class="button button--remove" data-id=${id}>delete</button></td>
+    </tr>
+    `
+    );
+  }
+}
+
+addBookToLibrary();
