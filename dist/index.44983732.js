13150dc78f0bbf7de75a6bdd2d3aa9e4050c2356
pav_nk <pav_nk@vk.com>
2024-05-31 16:46:48 +0300

    feat: the functionality of the application has been expanded

--- dist/index.44983732.js
+++ dist/index.44983732.js
@@ -1,9 +1,15 @@
 // nodes
 const form = document.body.querySelector(".entry-field");
 const booksContainer = document.body.querySelector(".library__items");
-const myLibrary = [];
-function Book() {
-// the constructor...
+let myLibrary = [];
+function Book(id, name, author, status) {
+    this.id = id;
+    this.name = name;
+    this.author = author;
+    this.status = status;
+    this.toggleStatus = function() {
+        this.status = this.status === "read" ? "unread" : "read";
+    };
 }
 function addBookToLibrary() {
     form.addEventListener("submit", (e)=>{
@@ -11,7 +17,9 @@ function addBookToLibrary() {
         const formData = new FormData(e.target);
         const newBookData = Object.fromEntries(formData);
         newBookData.id = myLibrary.length + 1;
-        myLibrary.push(newBookData);
+        const { id, name, author, status } = newBookData;
+        const newBook = new Book(id, name, author, status);
+        myLibrary.push(newBook);
         console.log(myLibrary);
         e.target.reset();
         renderBooks();
@@ -25,11 +33,23 @@ function renderBooks() {
     <tr class="library__item">
       <td>${name}</td>
       <td>${author}</td>
-      <td><span class="read-status">${status === "1" ? "read" : "unread"}</span></td>
+      <td><button class="button button--status">${status}</button></td>
       <td><button class="button button--remove" data-id=${id}>delete</button></td>
     </tr>
     `);
     }
+    addListeners();
+}
+function addListeners() {
+    const deleteButtons = Array.from(document.querySelectorAll(".button--remove"));
+    deleteButtons.forEach((button)=>{
+        button.addEventListener("click", ()=>{
+            console.log(button);
+            const currentId = button.dataset.id;
+            myLibrary = myLibrary.filter((book)=>book.id !== +currentId);
+            renderBooks();
+        });
+    });
 }
 addBookToLibrary();
 
--- src/script.js
+++ src/script.js
@@ -2,10 +2,16 @@
 const form = document.body.querySelector(".entry-field");
 const booksContainer = document.body.querySelector(".library__items");
 
-const myLibrary = [];
+let myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(id, name, author, status) {
+  this.id = id;
+  this.name = name;
+  this.author = author;
+  this.status = status;
+  this.toggleStatus = function () {
+    this.status = this.status === "read" ? "unread" : "read";
+  };
 }
 
 function addBookToLibrary() {
@@ -14,7 +20,9 @@ function addBookToLibrary() {
     const formData = new FormData(e.target);
     const newBookData = Object.fromEntries(formData);
     newBookData.id = myLibrary.length + 1;
-    myLibrary.push(newBookData);
+    const { id, name, author, status } = newBookData;
+    const newBook = new Book(id, name, author, status);
+    myLibrary.push(newBook);
     console.log(myLibrary);
     e.target.reset();
     renderBooks();
@@ -31,14 +39,27 @@ function renderBooks() {
     <tr class="library__item">
       <td>${name}</td>
       <td>${author}</td>
-      <td><span class="read-status">${
-        status === "1" ? "read" : "unread"
-      }</span></td>
+      <td><button class="button button--status">${status}</button></td>
       <td><button class="button button--remove" data-id=${id}>delete</button></td>
     </tr>
     `
     );
   }
+  addListeners();
+}
+
+function addListeners() {
+  const deleteButtons = Array.from(
+    document.querySelectorAll(".button--remove")
+  );
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      console.log(button);
+      const currentId = button.dataset.id;
+      myLibrary = myLibrary.filter((book) => book.id !== +currentId);
+      renderBooks();
+    });
+  });
 }
 
 addBookToLibrary();
